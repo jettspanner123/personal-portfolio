c6bf4bcc422fa499e48d81cc39070575315c22a2
jettspanner123 <uddeshyasingh12bsci@gmail.com>
2025-10-05 01:59:10 +0530

    Update: Added Favicon for the website

---
+++ .next/static/chunks/app_page_tsx_f3dbd526._.js
@@ -0,0 +1,24 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_locomotive-scroll_dist_locomotive-scroll_modern_mjs_85aa5625._.js",
+  "static/chunks/app_e5a4e3a8._.js",
+  "static/chunks/node_modules_next_0766a282._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_b0e39770._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_6b925d43._.js",
+  "static/chunks/node_modules_ogl_src_31d737fb._.js",
+  "static/chunks/node_modules_react-icons_bi_index_mjs_cba0e860._.js",
+  "static/chunks/node_modules_react-icons_gr_index_mjs_42e3d99f._.js",
+  "static/chunks/node_modules_react-icons_ci_index_mjs_e4544c82._.js",
+  "static/chunks/node_modules_react-icons_md_index_mjs_78df2b41._.js",
+  "static/chunks/node_modules_react-icons_fa_index_mjs_d2e2d7f5._.js",
+  "static/chunks/node_modules_react-icons_fa6_index_mjs_00e856c4._.js",
+  "static/chunks/node_modules_react-icons_rx_index_mjs_ecd194bc._.js",
+  "static/chunks/node_modules_react-icons_lib_74ccc930._.js",
+  "static/chunks/node_modules_three_build_three_core_df0bde7e.js",
+  "static/chunks/node_modules_three_build_three_module_230a9f89.js",
+  "static/chunks/node_modules_zod_v4_abc732fa._.js",
+  "static/chunks/node_modules_f4ce2c1c._.js"
+],
+    source: "dynamic"
+});
